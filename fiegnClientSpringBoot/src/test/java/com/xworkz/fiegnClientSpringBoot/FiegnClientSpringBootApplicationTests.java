067d77d709f085387953f771e28e942f5cf407ad
deepthins08 <deepthins.xworkz@gmail.com>
2024-10-27 13:25:46 +0530

    Spring Boot CRUD Operations

---
+++ fiegnClientSpringBoot/src/test/java/com/xworkz/fiegnClientSpringBoot/FiegnClientSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xworkz.fiegnClientSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiegnClientSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
